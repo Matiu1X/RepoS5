0c3b1ac59fe5b8ff7ac9dbafd80b00dedb973e50
brayan12y <brayan12y@hotmail.com>
2020-12-06 12:52:27 -0500

    nav-header-footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+
+    <ComponenteNav />
+    <ComponenteHeader />
+    <ComponenteFooter />
+
+ 
   </div>
 </template>
 
 <script>
 
+import ComponenteFooter from './components/ComponenteFooter.vue'
+import ComponenteNav from './components/ComponenteNav.vue'
+import ComponenteHeader from './components/ComponenteHeader.vue'
+
+
 export default {
   name: 'App',
+  components: {
+    ComponenteFooter,
+    ComponenteNav,
+    ComponenteHeader
+  }
 }
 </script>
 
