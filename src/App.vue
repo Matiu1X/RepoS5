56f8b983945d8eeeebe9eb80c78cccacbfc8cf40
Angela Agudelo A <angela.agudeloa@gmail.com>
2020-12-05 23:49:51 -0500

    servicios en vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <services-card></services-card>
   </div>
+  
 </template>
 
 <script>
+import ServicesCard from './components/ServicesCard.vue'
 
 export default {
+  components: { ServicesCard },
   name: 'App',
 }
 </script>
