d4c2e97b25ee2ce5da4e9774a0702ca32aa4b899
Angela Agudelo A <angela.agudeloa@gmail.com>
2020-12-06 18:00:43 -0500

    servicios dinamicos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <services-card></services-card>
+     <div class="text-center">
+          <h2 class="card-title" style="color: #49A078">OUR SERVICES</h2>	
+          <i class="ico-awesome ico-cogs color-secundary"></i>
+      </div>
+    <section id="services">
+        <div>	
+            <div class="d-flex p-2 bd-highlight d-flex justify-content-around ">  
+              <services-card v-for="service in servicios" :key="service.servtitulo" :services="service"></services-card>
+            </div>
+        </div>
+    </section>
   </div>
   
 </template>
 
 <script>
-import ServicesCard from './components/ServicesCard.vue'
+import ServicesCard from './components/ServicesCard.vue';
+import services from './assets/Services.js';
 
 export default {
   components: { ServicesCard },
   name: 'App',
+  data(){
+    return {
+      servicios: services}
+  }
 }
 </script>
 
--- src/components/ServicesCard.vue
+++ src/components/ServicesCard.vue
@@ -1,48 +1,21 @@
 <template>
-  <!-- Services -->
-    <section id="services">
+  <!-- Services -->   
+    <div class="border-0 card text-center" style="width: 400px;"> 
         <div class="text-center">
-            <h2 class="card-title" style="color: #49A078">OUR SERVICES</h2>	
-            <i class="ico-awesome ico-cogs color-secundary"></i>
+            <img style="width: 70px; margin-top: 10px" :src="services.servimage">
         </div>
-
-        <div>	
-            <div class="d-flex p-2 bd-highlight d-flex justify-content-around "> 
-                <div class="border-0 card text-center" style="width: 400px;"> 
-                    <div class="text-center">
-                        <img style="width: 70px; margin-top: 10px" src="../static/icon/service_1.svg" alt="domotica">
-                    </div>
-                    <div class="card-body">
-                        <h3 style="color: #49A078; height: 67.2px;">Domótica</h3>
-                        <p class="card-text">Control de iluminación, entretenimiento, seguridad y confort a su alcance.</p>
-                    </div>
-                </div>
-                <div class="border-0 card text-center" style="width: 400px">
-                    <div class="text-center">
-                        <img style="width: 70px; margin-top: 10px" src="../static/icon/service_2.svg" alt="ambiental">
-                    </div>
-                    <div class="card-body">
-                        <h3 style="color: #49A078; height: 67.2px;">Recolección de datos ambientales</h3>
-                        <p class="card-text">Optimizar el proceso de cultivar la tierra mediante el monitoreo.</p>
-                    </div>
-                </div>
-                <div class="border-0 card text-center" style="width: 400px">
-                    <div class="text-center">
-                        <img style="width: 70px; margin-top: 10px" src="../static/icon/service_3.svg" alt="datos">
-                    </div>
-                    <div class="card-body">
-                        <h3 style="color: #49A078; height: 67.2px;">Visualización de datos</h3>
-                        <p class="card-text">Representación gráfica de información y datos.</p>
-                    </div>
-                </div>
-            </div>    
-        </div>	
-	</section>
+        
+        <div class="card-body">
+            <h3 style="color: #49A078; height: 67.2px;">{{services.servtitulo}}</h3>
+            <p class="card-text">{{services.servdescrip}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'ServiceCard'
+    name: 'ServiceCard',
+    props: {services: Object},
 }
 </script>
 
